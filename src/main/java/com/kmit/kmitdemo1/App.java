d23c0a0e661b48d62dfda42f865f8608960b51d4
AakaashAK <aakaashmamidala@gmail.com>
2022-12-07 19:32:13 +0530

    SwitchGame.java program

--- src/main/java/com/kmit/kmitdemo1/App.java
+++ src/main/java/com/kmit/kmitdemo1/App.java
@@ -1,13 +1,25 @@
 package com.kmit.kmitdemo1;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Kmit!" );
+import java.util.*;
+class SwitchGame{
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(ss(s));
+    }
+    public static List<String> ss(String s){
+        List<String>l=new ArrayList<>();
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='+' && s.charAt(i+1)=='+')
+            {
+                String s1="";
+                if((i+1)!=s.length()-1)
+                    s1=s.substring(0,i)+"--"+s.substring(i+2);
+                else{
+                    s1=s.substring(0,i)+"--";
+                }
+                l.add(s1);
+            }    
+        }
+        return l;
     }
 }
